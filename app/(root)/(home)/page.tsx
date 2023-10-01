d5288c065056f73dd421e15fd185888b7fbe225a
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-10-01 18:53:24 +0300

    fix lint errors

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -9,7 +9,9 @@ import { getQuestions } from "@/lib/actions/question.action";
 import Link from "next/link";
 
 export default async function Home() {
-  const result = await getQuestions({});
+  const result: { questions: any[] } = (await getQuestions({})) || {
+    questions: [],
+  };
 
   return (
     <>
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -86,9 +86,9 @@ export async function deleteUser({ clerkId }: DeleteUserParams) {
     // and questions, answers, comments, etc.
 
     // get user question ids
-    const userQuestionIds = await Question.find({ author: user._id }).distinct(
-      "_id"
-    );
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
+    //   "_id"
+    // );
 
     // delete user questions
     await Question.deleteMany({ author: user._id });
