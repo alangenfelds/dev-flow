121e7c18df5183efc2d4eb1a5f4b15b6cbb5cc5f
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-10-05 08:43:25 +0300

    update community page

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,20 +1,19 @@
-import React from "react";
+import UserCard from "@/components/cards/UserCard";
 import Filter from "@/components/shared/Filter";
-import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { UserFilters } from "@/constants/filters";
 import { getAllUsers } from "@/lib/actions/user.action";
-import NoResult from "@/components/shared/NoResult";
 import Link from "next/link";
-import UserCard from "@/components/cards/UserCard";
 
-const CommunityPage = async () => {
-  const { users } = await getAllUsers({});
+const Page = async () => {
+  const result = await getAllUsers({});
+
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">All Users</h1>
 
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-        <LocalSearchBar
+        <LocalSearchbar
           route="/community"
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
@@ -29,13 +28,13 @@ const CommunityPage = async () => {
       </div>
 
       <section className="mt-12 flex flex-wrap gap-4">
-        {users.length > 0 ? (
-          users.map((user) => <UserCard key={user._id} user={user} />)
+        {result.users.length > 0 ? (
+          result.users.map((user) => <UserCard key={user._id} user={user} />)
         ) : (
           <div className="paragraph-regular text-dark200_light800 mx-auto max-w-4xl text-center">
             <p>No users yet</p>
             <Link href="/sign-up" className="mt-2 font-bold text-accent-blue">
-              Join to be the first
+              Join to be the first!
             </Link>
           </div>
         )}
@@ -44,4 +43,4 @@ const CommunityPage = async () => {
   );
 };
 
-export default CommunityPage;
+export default Page;
