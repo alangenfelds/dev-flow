4a328e96e4b8b6cb06292e0c5aaeaea5877963d9
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-10-08 10:17:35 +0300

    Save questions to collection

---
+++ app/(root)/collection/page.tsx
@@ -0,0 +1,66 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
+import { QuestionFilters } from "@/constants/filters";
+import { getSavedQuestions } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const result: { questions: any[] } = (await getSavedQuestions({
+    clerkId: userId,
+  })) || {
+    questions: [],
+  };
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchBar
+          route="/collection"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+
+        <Filter
+          filters={QuestionFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result?.questions?.length > 0 ? (
+          result.questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There’s no saved questions to show"
+            description="Save some questions to your collection by pressing the star icon &#11088;"
+            link="/"
+            linkTitle="Questions"
+          />
+        )}
+      </div>
+    </>
+  );
+}
