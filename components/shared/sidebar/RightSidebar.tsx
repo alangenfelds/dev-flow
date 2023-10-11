82fd5293f6c18a56a7c9dcbb7417e751bb4738e8
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-10-11 12:36:37 +0300

    Top questions and top tags

--- components/shared/sidebar/RightSidebar.tsx
+++ components/shared/sidebar/RightSidebar.tsx
@@ -2,60 +2,13 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import RenderTag from "../RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.actions";
 
-const hotQuestions = [
-  {
-    _id: "1",
-    title:
-      "Would it be appropriate to point out an error in another paper during a referee report?",
-  },
-  {
-    _id: "2",
-    title: "How can an airconditioning machine exist?",
-  },
-  {
-    _id: "3",
-    title: "Interrogated every time crossing UK Border as citizen",
-  },
-  {
-    _id: "4",
-    title: "Low digit addition generator",
-  },
-  {
-    _id: "5",
-    title: "What is an example of 3 numbers that do not make up a vector?",
-  },
-];
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
 
-const popularTags = [
-  {
-    _id: "1",
-    name: "Javascript",
-    totalQuestions: 85,
-  },
-  {
-    _id: "2",
-    name: "React.js",
-    totalQuestions: 45,
-  },
-  {
-    _id: "3",
-    name: "Next.js",
-    totalQuestions: 24,
-  },
-  {
-    _id: "4",
-    name: "Vue.js",
-    totalQuestions: 2,
-  },
-  {
-    _id: "5",
-    name: "Redux",
-    totalQuestions: 10,
-  },
-];
-
-const RightSidebar = () => {
   return (
     <section className="background-light900_dark200 light-border msx-sm:hidden custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col items-start  overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div className="">
@@ -64,7 +17,7 @@ const RightSidebar = () => {
           {hotQuestions.map((question) => (
             <Link
               key={question._id}
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
               <p className="body-medium text-dark500_light700">
