93c939621d5acd67904ce646a4713e5ad8217db6
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-10-17 15:56:41 +0300

    renamed tiny mce ENV variable

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -141,7 +141,7 @@ const Question = ({ type, mongoUserId, questionDetails }: Props) => {
 							</FormLabel>
 							<FormControl className='mt-3.5'>
 								<Editor
-									apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+									apiKey={process.env.NEXT_PUBLIC_EDITOR_TINY_MCE_API_KEY}
 									onInit={(evt, editor) => {
 										// @ts-ignore
 										editorRef.current = editor;
